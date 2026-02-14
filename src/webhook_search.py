6988bab9d0d660425c4f887660ea1296b7ea8690
yus <swiftcase@gmail.com>
2026-02-14 08:53:11 +0800

    Update webhook_search.py

--- src/webhook_search.py
+++ src/webhook_search.py
@@ -9,7 +9,7 @@ import sys
 import asyncio
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 import aiohttp
